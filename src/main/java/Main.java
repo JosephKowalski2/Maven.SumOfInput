3201306e2d1702a03b0f951069399e122b3cfa95
Joseph Kowalski <jkjr25@gmail.com>
2022-02-24 14:15:11 -0500

    Added changes to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,25 @@ public class Main {
     public static void main(String[] args){
         Main main_object = new Main();
 
-        System.out.println(main_object.sumOfNumbers(5));
+        //Early test case
+        //int x = 1 + 2 + 3 + 4;
+        //System.out.println(x);
+        //System.out.println(main_object.sumOfNumbers(100));
+        //System.out.println(main_object.gaussVersion(100));
+
+        
     }
 
     int sumOfNumbers (int n) {
-        return 0;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    int gaussVersion(int n) {
+        int sum = n * (n + 1) / 2;
+        return sum;
     }
 }
